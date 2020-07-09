9c3782f79fa8ac23a4e5f5caeae7960a4b558957
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-09 18:17:27 +0200

    get better version

--- Dice rolls/app.js
+++ Dice rolls/app.js
@@ -1,36 +1,56 @@
+//VERSION 0 
+
+// const buttonEl = document.querySelector(".button");
+
+// //add all img to an array of objects
+// const imgArr = [];
+// imgArr.push(document.querySelector(".dice1"));
+// imgArr.push(document.querySelector(".dice2"));
+// imgArr.push(document.querySelector(".dice3"));
+// imgArr.push(document.querySelector(".dice4"));
+// imgArr.push(document.querySelector(".dice5"));
+// imgArr.push(document.querySelector(".dice6"));
+
+
+// //get random number from 0 to 5 =>> that'why it is * 6 and FLOOR
+// function getRandomIntInclusive() {
+//     return Math.floor((Math.random() * 6));
+//   }
+
+// function main() {
+//     buttonEl.addEventListener("click", roll);
+
+//     function roll() {
+//         let randomNum = getRandomIntInclusive();
+//         for (let i=0; i<6; i++){
+//             if (i === randomNum) {
+//                 imgArr[i].classList.add("show");
+//             }
+//             else {
+//                 imgArr[i].classList.remove("show");
+//             }
+//         }
+//     }
+// }
+
+// main();
+
+
+//VERSION 1
+
 const buttonEl = document.querySelector(".button");
+const diceImg = document.querySelector(".diceimg");
 
-//add all img to an array of objects
-const imgArr = [];
-imgArr.push(document.querySelector(".dice1"));
-imgArr.push(document.querySelector(".dice2"));
-imgArr.push(document.querySelector(".dice3"));
-imgArr.push(document.querySelector(".dice4"));
-imgArr.push(document.querySelector(".dice5"));
-imgArr.push(document.querySelector(".dice6"));
-
-
-//get random number
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+//get random number from 0 to 5 =>> that'why it is * 6 and FLOOR
+function getRandomIntInclusive() {
+    return 1 + Math.floor(Math.random() * 6);
   }
 
 function main() {
-    buttonEl.addEventListener("click", roll);
-
-    function roll() {
-        let randomNum = getRandomIntInclusive(0, 5);
-        for (let i=0; i<6; i++){
-            if (i === randomNum) {
-                imgArr[i].classList.add("show");
-            }
-            else {
-                imgArr[i].classList.remove("show");
-            }
-        }
-    }
+    buttonEl.addEventListener("click", function() {
+        let randomNum = getRandomIntInclusive();
+        diceImg.src = `images/dice${randomNum}.png`;
+    });
 }
 
 main();
