66cc3c4a5d45986840e807d6807c23dbf28bf84c
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-13 16:30:53 +0200

    some changes

---
+++ To-do-list/app.js
@@ -0,0 +1,10 @@
+const todoInputEl = document.querySelector("todo__input");
+const todoListEl = document.querySelector("todo__list");
+const todoItemEl = document.querySelectorAll("todo__item");
+
+function addListItem () {
+    console.log("checkt");
+}
+
+addListItem();
+
