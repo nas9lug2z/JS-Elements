50d09f23a2aabf4295260004a5628781904df7a1
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-13 16:54:14 +0200

    todo list adds4

--- To-do-list/app.js
+++ To-do-list/app.js
@@ -1,10 +1,26 @@
-const todoInputEl = document.querySelector("todo__input");
-const todoListEl = document.querySelector("todo__list");
-const todoItemEl = document.querySelectorAll("todo__item");
+const todoInputEl = document.querySelector(".todo-input");
+const todoListEl = document.querySelector(".todo__list");
+const todoItemEl = document.querySelectorAll(".todo__item");
+
 
 function addListItem () {
-    console.log("checkt");
+    todoInputEl.addEventListener("keypress", function (event) {
+        if (event.keyCode === 13) {
+            let newListItem = createListItem(todoInputEl.value);
+            todoListEl.appendChild(newListItem);
+            todoInputEl.value = "";
+        }
+    })
 }
 
+
+function createListItem (text) {
+    const newListEl = document.createElement("li");
+    newListEl.textContent = text;
+    newListEl.classList.add("todo__item");
+    return newListEl;
+}
+
+createListItem();
 addListItem();
 
