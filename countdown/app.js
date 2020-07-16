996e296a2e386d0702d13c9c6f9637f1b3789542
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-16 11:34:49 +0200

    checking

--- countdown/app.js
+++ countdown/app.js
@@ -1,29 +1,29 @@
-let days = Number(document.querySelector(".timer__day").innerHTML);
-let hours = Number(document.querySelector(".timer__hour").innerHTML)
+let days = Number(document.querySelector(".timer__days").innerHTML);
+let hours = Number(document.querySelector(".timer__hours").innerHTML)
 
 
 const today = Date();
 console.log(today)
 console.log(hours)
 
-function countDays() {
+function countTime(time, range) {
     setInterval(function() {
-        if (days === 0) {
-            document.querySelector(".timer__day").nextElementSibling.innerHTML = "Days";
+        if (time === 0) {
+            document.querySelector(`.timer__${time}`).nextElementSibling.innerHTML = "Days";
             clearInterval(countDays);
         }
         else if (days === 2) {
             days--;
-            document.querySelector(".timer__day").nextElementSibling.innerHTML = "Day";
-            document.querySelector(".timer__day").innerHTML =  "0" + days;
+            document.querySelector(".timer__days").nextElementSibling.innerHTML = "Day";
+            document.querySelector(".timer__days").innerHTML =  "0" + days;
         }
         else if (days < 10) {
             days--;
-            document.querySelector(".timer__day").innerHTML =  "0" + days;
+            document.querySelector(".timer__days").innerHTML =  "0" + days;
         }
         else {
             days--;
-            document.querySelector(".timer__day").innerHTML = days;
+            document.querySelector(".timer__days").innerHTML = days;
         }
     }, 1000);
 }
@@ -32,24 +32,24 @@ function countDays() {
 function countHours() {
     setInterval(function() {
         if (hours === 0) {
-            document.querySelector(".timer__hour").nextElementSibling.innerHTML = "Hours";
+            document.querySelector(".timer__hours").nextElementSibling.innerHTML = "Hours";
             clearInterval(countHours);
         }
         else if (hours === 2) {
             hours--;
-            document.querySelector(".timer__hour").nextElementSibling.innerHTML = "Hour";
-            document.querySelector(".timer__hour").innerHTML =  "0" + hours;
+            document.querySelector(".timer__hours").nextElementSibling.innerHTML = "Hour";
+            document.querySelector(".timer__hours").innerHTML =  "0" + hours;
         }
         else if (hours < 10) {
             hours--;
-            document.querySelector(".timer__hour").innerHTML =  "0" + hours;
+            document.querySelector(".timer__hours").innerHTML =  "0" + hours;
         }
         else {
             hours--;
-            document.querySelector(".timer__hour").innerHTML = hours;
+            document.querySelector(".timer__hours").innerHTML = hours;
         }
     }, 1000);
 }
 
 countHours();
-countDays();
+countTime(days, 07);
