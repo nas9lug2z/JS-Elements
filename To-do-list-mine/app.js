b9ff661da320616be2c88565cd1fec2bd499dec5
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-09-02 16:20:43 +0200

    items can be added and removed

--- To-do-list-mine/app.js
+++ To-do-list-mine/app.js
@@ -1,38 +1,27 @@
 const task = document.querySelectorAll(".task");
 const addInput = document.querySelector("#add");
 const ulList = document.querySelector("ul");
+const searchInput = document.querySelector("#search");
 
-
-
-
-
-function toggleDone() {
-    for (let item of task){
-        item.addEventListener('click', (e) => {
-                if (e.target.classList.contains("cross") !== true) {
-                    item.firstChild.classList.toggle("done")
+ulList.addEventListener('click', (e) => {
+                if (e.target.classList.contains("cross") === true) {
+                    e.target.parentNode.style.display = "none";
                 }
-                else {
-                    item.parentNode.removeChild(item);
+                else if (e.target.classList.contains("task") === true) {
+                    e.target.firstChild.classList.toggle("done");
+                }
+                else if (e.target.classList.contains("text") === true) {
+                    e.target.classList.toggle("done");
                 }
         });
-    }
-}
 
-function addNewTask() {
-        addInput.addEventListener('keypress', (e) => {
+addInput.addEventListener('keypress', (e) => {
             if (addInput.value !== "" && e.keyCode === 13) {
                 let newTask = document.createElement('li');
                 newTask.className = 'task';
                 newTask.innerHTML = '<span class="text">' + addInput.value + '</span><span class="cross">X</span>'
                 ulList.appendChild(newTask);
-                console.log(newTask);
-                toggleDone();
+                addInput.value = "";
             }
         })
-   
 
-}
-
-toggleDone();
-addNewTask();
